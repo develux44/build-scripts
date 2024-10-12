11a9ddfdc070d90eb8eab4d375d17a8ff759a65f
develux <103586125+develux44@users.noreply.github.com>
2024-10-12 13:42:56 +0500

    Create crave-aospa-build-fog.sh

---
+++ crave-aospa-build-fog.sh
@@ -0,0 +1,7 @@
+rm -rf .repo/local_manifests
+repo init -u https://github.com/AOSPA/manifest -b uvite --depth=1
+/opt/crave/resync.sh
+rm -rf vendor/aospa/products
+git clone https://github.com/develux44/vendor_aospa-addfog.git vendor/aospa/products
+git clone https://github.com/alternoegraha/wwy_kernel_xiaomi_fog_rebase.git -b another-reset kernel/xiaomi/fog --depth=1
+./rom-build.sh fog
